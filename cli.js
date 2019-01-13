5e622dedfd1e8f1449584f09813ed1c79860cde1
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2019-01-13 20:07:22 +0100

    Make CLI work

--- cli.js
+++ cli.js
@@ -5,12 +5,11 @@ const { transformJsStringToMermaidString } = require(".");
 
 const [, , ...args] = process.argv;
 
-console.log(`Hello world ${args}`);
+// console.log(`Hello world ${args}`);
 
 function main(args) {
   const string = fs.readFileSync(args[0], { encoding: "utf8" });
-  //   console.log(string);
-  transformJsStringToMermaidString(string);
+  console.log(transformJsStringToMermaidString(string));
 }
 
 main(args);
